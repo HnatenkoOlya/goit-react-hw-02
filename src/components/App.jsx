8e0d7fe8e27ca950d2e487c2a899f7280c5eebc1
HnatenkoOlya <gnatenkoolgaig1210@gmail.com>
2025-03-28 11:44:51 +0100

    2/2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import Description from './description/Description.jsx';
 import Options from './options/Options.jsx'
 import Feedback from './feedback/Feedback.jsx'
+import Notification from './notification/Notification.jsx'
 import './App.css'
 
 export default function App () {
@@ -11,11 +12,27 @@ export default function App () {
     bad: 0
   }
   )
+  const updateFeedback = (feedbackType) => {
+    setRating(prevState => ({
+      ...prevState,
+      [feedbackType]: prevState[feedbackType] + 1
+    }));
+    };
+    const resetFeedback = () => {
+      setRating({
+        good: 0,
+        neutral: 0,
+        bad: 0
+      }
+      );
+    };
+  const totalFeedback = rating.good + rating.neutral + rating.bad
+
   return (
     <div>
       <Description/>
-      <Options option={setRating}/>
-      <Feedback feedback={rating}/>
+      <Options updateFeedback={updateFeedback} resetFeedback={resetFeedback} totalFeedback={totalFeedback}/>
+      {totalFeedback > 0 ?  <Feedback feedback={rating}/> : <Notification/>}
     </div>
   )
 }
