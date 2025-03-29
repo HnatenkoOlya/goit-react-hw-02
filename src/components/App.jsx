e754cbe2efee116d3447e3a8956926d1f73337cd
HnatenkoOlya <gnatenkoolgaig1210@gmail.com>
2025-03-29 17:10:33 +0100

    2/3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Description from './description/Description.jsx';
 import Options from './options/Options.jsx'
 import Feedback from './feedback/Feedback.jsx'
@@ -6,12 +6,32 @@ import Notification from './notification/Notification.jsx'
 import './App.css'
 
 export default function App () {
-  const [rating, setRating]=useState({
+  const [rating, setRating] = useState (() => {
+    const savedRating = window.localStorage.getItem("saved-rating");
+    return savedRating ? JSON.parse(savedRating) : {
+      good: 0,
+      neutral: 0,
+      bad: 0
+    }
+  })
+  
+  
+  
+  /*({
     good: 0,
     neutral: 0,
     bad: 0
+  }  => {
+    const savedRating = window.localStorage.getItem("saved-rating");
+    if (savedRating !== null) {
+      return JSON.parse(savedRating);
+    }
+    return {};
   }
-  )
+  );*/
+  useEffect(() => {
+    window.localStorage.setItem("saved-rating", JSON.stringify(rating));
+  }, [rating]);
   const updateFeedback = (feedbackType) => {
     setRating(prevState => ({
       ...prevState,
@@ -27,12 +47,14 @@ export default function App () {
       );
     };
   const totalFeedback = rating.good + rating.neutral + rating.bad
+  const positiveFeedback = Math.round((rating.good / totalFeedback) * 100)
+
 
   return (
-    <div>
+    <div className='divA'>
       <Description/>
       <Options updateFeedback={updateFeedback} resetFeedback={resetFeedback} totalFeedback={totalFeedback}/>
-      {totalFeedback > 0 ?  <Feedback feedback={rating}/> : <Notification/>}
+      {totalFeedback > 0 ?  <Feedback feedback={rating} totalFeedback={totalFeedback} positiveFeedback={positiveFeedback}/> : <Notification/>}
     </div>
   )
 }
--- src/components/options/Options.jsx
+++ src/components/options/Options.jsx
@@ -1,10 +1,11 @@
+import style from "./Options.module.css"
 export default function Options ({updateFeedback, totalFeedback, resetFeedback}) {
     return (
         <div>
-            <button onClick={() => updateFeedback("good")}>Good </button>
-            <button onClick={() => updateFeedback("neutral")}>Neutral</button>
-            <button onClick={() => updateFeedback("bad")}>Bad</button>
-            {totalFeedback > 0 && <button onClick={resetFeedback}>Reset</button>}
+            <button className={style.btnOp} onClick={() => updateFeedback("good")}>Good </button>
+            <button className={style.btnOp} onClick={() => updateFeedback("neutral")}>Neutral</button>
+            <button className={style.btnOp} onClick={() => updateFeedback("bad")}>Bad</button>
+            {totalFeedback > 0 && <button className={style.btnOp} onClick={resetFeedback}>Reset</button>}
         </div>
     )
 }
